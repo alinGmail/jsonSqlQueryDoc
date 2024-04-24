1218ff63a28bba6e05ef81b680936e8b08746033
alin <a2231232@gmail.com>
2024-04-24 21:08:53 +0800

    add url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -66,7 +66,7 @@ const config: Config = {
         {
           position: 'left',
           label: 'download in App Store',
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://apps.apple.com/en/app/jsonsqlquery/id1596097026?l=en-GB&mt=12',
         }
       ],
     },
